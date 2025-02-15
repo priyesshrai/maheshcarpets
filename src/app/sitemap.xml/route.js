a7a50b179da9a0bc82cf5ac20ae72e093b36e80e
PriyeshRai369 <priyeshrai369@gmail.com>
2025-02-15 10:27:46 +0530

    fix: handle errors when fetching sitemap and comment out Map component

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,11 +1,23 @@
 export async function GET() {
+  try {
     const res = await fetch("https://www.maheshcarpets.com/api/sitemap");
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    const contentType = res.headers.get("Content-Type");
     const sitemap = await res.text();
-  
+
     return new Response(sitemap, {
       headers: {
         "Content-Type": "text/xml",
       },
     });
+  } catch (error) {
+    return new Response(`Error fetching sitemap: ${error.message}`, {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
   }
-  
+}
