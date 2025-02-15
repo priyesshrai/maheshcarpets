83f45f8db533e6a1ca87c5529b383f1c1c56d137
PriyeshRai369 <priyeshrai369@gmail.com>
2025-02-15 13:52:08 +0530

    feat: add generateMetadata function to product pages for improved SEO

--- src/app/collections/bespoke/[productId]/page.jsx
+++ src/app/collections/bespoke/[productId]/page.jsx
@@ -3,10 +3,22 @@ import Product from "./Product";
 
 export default async function Page({ params }) {
   const { productId } = await params;
-
+  
   if (!productId) {
     return <p>Error: Product ID not found.</p>;
   }
 
   return <Product productId={productId} />;
+}
+export async function generateMetadata({ params }) {
+  const { productId } = await params;
+  const title = productId
+    .split("-")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  
+  return {
+    title: title,
+    description: `${title} by Mahesh Carpets, leaders in art of Carpet Manufacturing in India, with 50 years of expertise in Varanasi. Our portfolio encompasses an extensive array of handcrafted indoor rugs.`,
+  }
 }
--- src/app/collections/shaggy/[productId]/page.jsx
+++ src/app/collections/shaggy/[productId]/page.jsx
@@ -10,3 +10,15 @@ export default async function Page({ params }) {
 
   return <Product productId={productId} />;
 }
+export async function generateMetadata({ params }) {
+  const { productId } = await params;
+  const title = productId
+    .split("-")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+  
+  return {
+    title: title,
+    description: `${title} by Mahesh Carpets, leaders in art of Carpet Manufacturing in India, with 50 years of expertise in Varanasi. Our portfolio encompasses an extensive array of handcrafted indoor rugs.`,
+  }
+}
