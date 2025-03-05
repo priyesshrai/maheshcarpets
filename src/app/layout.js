83b3314ec682db1f59c51b810cab8043ce026484
Semicolon-Wiz <semicolonwizards@gmail.com>
2025-03-05 18:33:37 +0530

    Schema add in Head

--- src/app/layout.js
+++ src/app/layout.js
@@ -58,6 +58,43 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="G2XKjAWAeAOJgLFfUkfX9gTQYENs4xPsZxr-1h6g5Kc"
         />
+     <script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Mahesh Carpets",
+  "image": "https://www.maheshcarpets.com/images/our-story.jpg",
+  "@id": "",
+  "url": "https://www.maheshcarpets.com/",
+  "telephone": "09918639000",
+  "priceRange": "$$",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "90 D, JAGATPUR, G.T. ROAD",
+    "addressLocality": "Varanasi",
+    "postalCode": "221302",
+    "addressCountry": "IN"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": 25.2743273,
+    "longitude": 82.8721105
+  },
+  "openingHoursSpecification": {
+    "@type": "OpeningHoursSpecification",
+    "dayOfWeek": [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ],
+    "opens": "09:30",
+    "closes": "18:00"
+  } 
+}
+</script>       
       </head>
       <body
         className={`${jura.variable} ${akayaKanadaka.variable} ${raleway.variable} ${yatraOne.variable} ${ibmPlexSans.variable}`}
