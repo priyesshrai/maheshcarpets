6321382aa800c8b1e84a06e0d4e4d64b88533025
priyesshrai <priyeshrai369@gmail.com>
2025-03-25 16:52:05 +0530

    Redirect to admin dashboard on page load and clean up component structure

--- src/app/(admin)/admin/page.jsx
+++ src/app/(admin)/admin/page.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+'use client'
+import React,{ useEffect } from 'react'
 
 export default function page() {
+  useEffect(() => {
+    window.location.href = '/admin/dashboard'
+  }, [])
   return (
-    <div>page</div>
+    <div></div>
   )
 }
