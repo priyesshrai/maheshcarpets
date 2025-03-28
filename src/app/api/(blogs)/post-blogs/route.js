ccbeafc6d888378d2793625c0341e280a4e376f0
priyesshrai <priyeshrai369@gmail.com>
2025-03-28 14:09:16 +0530

    Refactor blog image handling in POST route: update image upload logic to check for existing images and streamline the update process

--- src/app/api/(blogs)/post-blogs/route.js
+++ src/app/api/(blogs)/post-blogs/route.js
@@ -47,11 +47,6 @@ export const POST = async (req) => {
 
     const slug = createSlug(title);
     let imageUrl = null;
-    if (image) {
-      const buffer = Buffer.from(await image.arrayBuffer());
-      const fileName = `${Date.now()}-${image.name.replace(/\s+/g, "_")}`;
-      imageUrl = await fileUpload(buffer, fileName);
-    }
 
     if (blogId) {
       const [existingBlog] = await pool.execute(
@@ -60,6 +55,21 @@ export const POST = async (req) => {
       );
 
       if (existingBlog.length > 0) {
+        const getActualImageName = (url) => {
+          if (!url) return null;
+          const match = url.match(/\/(\d+-([^.]+\.[a-z]+))/i);
+          return match ? match[2] : null;
+        };
+
+        const existingImg = getActualImageName(existingBlog[0]?.blog_img);
+        if (image && existingImg !== image.name) {
+          const buffer = Buffer.from(await image.arrayBuffer());
+          const fileName = `${Date.now()}-${image.name.replace(/\s+/g, "_")}`;
+          imageUrl = await fileUpload(buffer, fileName);
+        } else {
+          imageUrl = existingBlog[0]?.blog_img;
+        }
+
         const updateQuery = `
           UPDATE blogs 
           SET blog_title = ?, blog_content = ?, blog_img = ?, metaTitle = ?, metaDesc = ?, markdownContent = ?, 
@@ -80,18 +90,24 @@ export const POST = async (req) => {
           {
             success: true,
             message: "Blog updated successfully",
-            blogId: existingBlog[0].id,
+            blogId: existingBlog[0].blog_id,
+            imageUrl,
           },
           { status: 200 }
         );
       }
     }
 
+    if (image) {
+      const buffer = Buffer.from(await image.arrayBuffer());
+      const fileName = `${Date.now()}-${image.name.replace(/\s+/g, "_")}`;
+      imageUrl = await fileUpload(buffer, fileName);
+    }
+
     const query = `
       INSERT INTO blogs 
       (blog_title, blog_content, blog_slug, blog_img, metaTitle, metaDesc, markdownContent) 
-      VALUES (?, ?, ?, ?, ?, ?, ?)
-    `;
+      VALUES (?, ?, ?, ?, ?, ?, ?)`;
     const values = [
       title,
       content,
