f64c24f3117cc1229252face8d6d211952fc9e65
priyesshrai <priyeshrai369@gmail.com>
2025-04-05 12:00:20 +0530

    Enhance GET blogs API: add empty response handling and trim blog content

--- src/app/api/(blogs)/get-blogs/route.js
+++ src/app/api/(blogs)/get-blogs/route.js
@@ -5,10 +5,28 @@ export async function GET() {
     const query = "SELECT * FROM blogs ORDER BY created_at DESC";
     const [blogs] = await pool.execute(query);
 
+    if (blogs.length === 0) {
+      return Response.json(
+        { success: false, message: "No blogs found" },
+        { status: 404 }
+      );
+    }
+
+    const data = blogs.map((blog) => {
+      return {
+        blog_id: blog.blog_id,
+        blog_title: blog.blog_title,
+        blog_content: blog.blog_content.trim().substring(0, 250) + "...",
+        blog_img: blog.blog_img,
+        blog_slug: blog.blog_slug,
+        createdAt: blog.created_at,
+      };
+    });
+
     return Response.json(
       {
         success: true,
-        data: blogs,
+        data: data,
       },
       { status: 200 }
     );
